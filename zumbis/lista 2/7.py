53217aba61b46cea4cf52d46e5825a92e02c82fe
sunshine <solitudelabhacking@gmail.com>
2018-10-29 18:17:18 -0300

    v

--- zumbis/lista 2/7.py
+++ zumbis/lista 2/7.py
@@ -5,11 +5,22 @@ latas de 18 litros, que custam R$ 80,00. Informe ao usuário a quantidades de
 latas de tinta a serem compradas e o preço total. Obs. : somente são vendidos 
 um número inteiro de latas."""
 
-area = int(input("Área que será pintata\n"))
+metros_area = int(input("Área que será pintada\n"))
 
-for i in range(area):
-	
+litros = metros_area * 3 # para cada metro, 3 litros
+lata = litros // 18 # pegando a quantidade de latas baseada nos litros
 
+if lata == 0: #and litros < 18: # se a quantidade de litros for maior que zero e diferente de 18, lata recebe + 1
+	lata += 1 # isso evita que caso a quantidade de litros necessários sejam menores que 18, as latas fiquem zeradas
+
+
+preco = lata * 80
+
+print("Metros que serão pintados", metros_area)
+print("Litros necessários", litros)
+print("Latas necessárias", lata)
+print("Preço", preco)
+# 1 lada para cada 54 metros
 # 1 litro para cada 3 metros
 # lata = 18 litros 
 # preço 80
